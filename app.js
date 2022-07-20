c469bca7e42250a86d7289c0bd2a3d0b486d7c56
Divxsh <guptadivash70@gmail.com>
2022-07-20 14:07:04 +0530

    make it better

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var cors = require('cors');
 const authMiddleware = require('./middlewares/AuthMiddleware');
 const { apiRoute, protectedRoute } = require('./routes/api');
+const res = require('express/lib/response');
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
 app.use('/api/',apiRoute);
 app.use('/api/', authMiddleware, protectedRoute)
+app.get("/",(req,res)=>{res.send({msg: "Hello"})});
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>console.log(`Server is Listening at ${port}`))
